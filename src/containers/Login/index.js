7bd8f80ee100887c244d1d509d250c5e6268fb1f
FIRST_NAME LAST_NAME <matt.o@smallsparksoftware.co.uk>
2020-06-08 04:46:20 +0500

    Quick Fix

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -19,7 +19,7 @@ function Login() {
 
   const loginUser = () => {
     axios
-      .post("/api/api/users/login", {
+      .post("/api/users/login", {
         email: values.email,
         password: values.password,
       })
